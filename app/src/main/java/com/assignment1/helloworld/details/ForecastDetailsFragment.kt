331bab5e347b4248685659b6ebdc02b4e9a47c74
darienfayt <phamphuongloan@gmail.com>
2020-06-22 04:08:56 -0700

    update

--- app/src/main/java/com/assignment1/helloworld/details/ForecastDetailsFragment.kt
+++ app/src/main/java/com/assignment1/helloworld/details/ForecastDetailsFragment.kt
@@ -3,18 +3,36 @@ package com.assignment1.helloworld.details
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.navArgs
+import coil.api.load
 import com.assignment1.helloworld.*
+import com.assignment1.helloworld.databinding.FragmentForecastDetailsBinding
 import kotlinx.android.synthetic.*
 import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class ForecastDetailsFragment : Fragment() {
 
     private val args: ForecastDetailsFragmentArgs by navArgs()
+
+    private lateinit var viewModelFactory: ForecastDetaisViewModelFactory
+    private val viewModel = ForecastDetailsViewModel by viewModels(
+        factoryProducer = { viewModelFactory }
+    )
+
+    private var _binding: FragmentForecastDetailsBinding? = null
+    //this properties only valid between onCreateView and onDestroyView
+    private val binding get() = _binding!!
 
     private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
 
@@ -23,16 +41,27 @@ class ForecastDetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val layout = inflater.inflate(R.layout.fragment_forecast_details, container, false)
-
+        _binding = inflater.inflate(inflater, container, false)
+        viewModelFactory = ForecastDetaisViewModelFactory(args)
         tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
 
-        val tempText = layout.findViewById<TextView>(R.id.tempText)
-        val descriptionText = layout.findViewById<TextView>(R.id.descriptionText)
+        return binding.root
+    }
 
-        tempText.text = formatTempForDisplay(args.temp, tempDisplaySettingManager.getTempDisplaySetting())
-        descriptionText.text = args.description
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewStateObserver = Observer<ForecastDetailsViewState> { viewState ->
+            //updaate the UI
+            binding.tempText.text = formatTempForDisplay(viewState.temp, tempDisplaySettingManager.getTempDisplaySetting())
+            binding.descriptionText.text = viewState.description
+            binding.dateText.text = viewState.date
+            binding.forecastIcon.load((viewState.iconUrl))
+        }
+        viewModel.viewState.observe(viewLifecycleOwner, viewStateObserver)
+    }
 
-        return layout
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/assignment1/helloworld/details/ForecastDetailsViewState.kt
@@ -0,0 +1,10 @@
+package com.assignment1.helloworld.details
+
+import java.util.*
+
+class ForecastDetailsViewState (
+    val temp: Float,
+    val description: String,
+    val date: String,
+    val iconUrl: String
+)
